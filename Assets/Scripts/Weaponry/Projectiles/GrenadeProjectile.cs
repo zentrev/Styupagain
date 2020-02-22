0659af490996bfdeb1c5e75273677deab533ca21
Spencer Southam <33136960+SSoutham@users.noreply.github.com>
2020-02-22 16:40:55 -0700

    Intro to New Repo
    Probably Broke things

--- Assets/Scripts/Weaponry/Projectiles/GrenadeProjectile.cs
+++ Assets/Scripts/Weaponry/Projectiles/GrenadeProjectile.cs
@@ -20,10 +20,8 @@ public class GrenadeProjectile : ProjectileBase
         List<Health> hpObjs = Physics.OverlapSphere(transform.position, Range).Where(h => h.GetComponent<Health>()).Select(h => h.GetComponent<Health>()).ToList();
         foreach (Health c in hpObjs)
         {
-            if (c.tag == "Player")
-            {
-                c.DealDamage(Damage); //* modifier
-            }
+            c.DealDamage(Damage); //* modifier
+
         }
         Instantiate(ExplosionPrefab, transform.position, transform.rotation, null);
         Destroy(gameObject);
