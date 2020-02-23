9884bb4266fed695ed3049108f698afea078c46d
Kaedian <38064751+Kaedian-Maxfield@users.noreply.github.com>
2020-02-22 17:42:25 -0700

    weapon stuff?
    I think?

--- Assets/Scripts/Weaponry/WeaponManager.cs
+++ Assets/Scripts/Weaponry/WeaponManager.cs
@@ -11,11 +11,6 @@ public class WeaponManager : MonoBehaviour
     [SerializeField] GameObject MolotovProjectile;
     [SerializeField] GameObject GrenadeProjectile;
     [SerializeField] GameObject MissileProjectile;
-    [Header("Modifier Prefabs")]
-    [SerializeField] GameObject RicochetModifier;
-    [SerializeField] GameObject ThreeWayModifier;
-    [SerializeField] GameObject ClusterModifier;
-    [SerializeField] GameObject PierceModifier;
 
     [Header("Player's Selections")]
     public ProjectileBase.EProjectileType projectileSelected;
@@ -26,6 +21,40 @@ public class WeaponManager : MonoBehaviour
 
     public void SetUpSelections()
     {
+        Instantiate(WeaponBase, weaponSpawnPosition).GetComponent<WeaponBase>().weaponType = weaponSelected;
+        ProjectileBase b = null;
+        switch (projectileSelected)
+        {
+            case ProjectileBase.EProjectileType.BULLET:
+                b = Instantiate(BulletProjectile).GetComponent<ProjectileBase>();
+                break;
+            case ProjectileBase.EProjectileType.GRENADE:
+                b = Instantiate(GrenadeProjectile).GetComponent<ProjectileBase>();
+                break;
+            case ProjectileBase.EProjectileType.MISSILE:
+                b = Instantiate(MissileProjectile).GetComponent<ProjectileBase>();
+                break;
+            case ProjectileBase.EProjectileType.MOLOTOV:
+                b = Instantiate(MolotovProjectile).GetComponent<ProjectileBase>();
+                break;
+        }
+
+        switch (modifierSelected)
+        {
+            case ModifierBase.EModifier.CLUSTER:
+                b.gameObject.AddComponent<ClusterModifier>().m_projectile = b;
+                break;
+            case ModifierBase.EModifier.RICOCHET:
+                b.gameObject.AddComponent<RicochetModifier>().m_projectile = b;
+                break;
+            case ModifierBase.EModifier.THREEWAY:
+                b.gameObject.AddComponent<ThreeWayModifier>().m_projectile = b;
+                break;
+            case ModifierBase.EModifier.PIERCE:
+                b.gameObject.AddComponent<PierceModifier>().m_projectile = b;
+                break;
+        }
+
         //Get Selections (from manager?)
         //Instantiate game objects
         //Link them together
